7a35a1c948328a266548e5bf45f8915212bb8063
Tomi <mariamismael904@gmail.com>
2022-09-05 23:02:39 +0100

    added the search component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
-import {Navbar} from './components/Navbar';
-import {Feed} from './components/Feed';
-import {VideoDetail} from './components/VideoDetail';
-import {ChannelDetail} from './components/ChannelDetail';
-import {SearchFeed} from './components/SearchFeed';
-
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
 
 
 const App = () => (
   <BrowserRouter>
-  <Box sx={{ backgroundColor: '#0000'}}>
+  <Box sx={{ backgroundColor: '#000'}}>
 <Navbar />
 <Routes>
   <Route path='/' exact element={<Feed />} />
